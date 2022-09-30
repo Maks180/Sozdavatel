4e82408515a36134c703b5e1f68f97caceee2653
Maks180 <maksim.sckoropistsev@ya.ru>
2022-09-30 19:40:57 +0300

    Refactoring

--- block3/task1/index.php
+++ block3/task1/index.php
@@ -1,18 +1,17 @@
-
 <?php
-function get_income_on_deposit($n, $m, $p){
-    $current_data = new DateTime(date("j-n-Y"));
-	$future_date = new DateTime(date("j-n-Y", strtotime("+$m month")));
-	$interval = date_diff($current_data, $future_date);
-	$interval1 = $future_date->diff($current_data);
-	
+function getIncome($n, $m, $p)
+{
+    $currentDate = new DateTime(date("j-n-Y"));
+	$futureDate = new DateTime(date("j-n-Y", strtotime("+$m month")));
+	$interval = date_diff($currentDate, $futureDate);
+
     return  $n + $n * $interval->format('%a') * $p / 100 / 365;
 }
-$deposit_amount = 350000;
-$time = 9;
-$annual_interest = 4.7;
 
-
-echo "Результат: " . get_income_on_deposit($deposit_amount, $time, $annual_interest);
+$time 			= 9;
+$depositAmount  = 350000;
+$annualInterest = 4.7;
+echo 'Результат: '
+ . getIncome($depositAmount, $time, $annualInterest);
 
 ?>
