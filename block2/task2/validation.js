62ac33a944f50cd05b1b758f3648483c6b8d7b1a
Maks180 <maksim.sckoropistsev@ya.ru>
2022-10-03 15:08:13 +0300

    Refactoring task2.2

--- block2/task2/validation.js
+++ block2/task2/validation.js
@@ -5,12 +5,12 @@ let form = document.querySelector('.js-form'),
   
 
 function validateEmail(email) {
-    let regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let regexp = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
     return regexp.test(String(email).toLowerCase());
 }    
 
 function validatePhone(phone) {
-    let regexp = /^[0-9\s]*$/;
+    let regexp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
     return regexp.test(String(phone));
 }
 
